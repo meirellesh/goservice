f0983754e26d3d368e85bc31862f70e20f939529
Henrique <meirelles.henrique@outlook.com>
2023-08-24 14:04:26 -0300

    Adicionando métodos para alterar senha - AuthController

--- src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -1,9 +1,10 @@
 package com.soulcode.goserviceapp.controller;
-
 import com.soulcode.goserviceapp.domain.Cliente;
+import com.soulcode.goserviceapp.service.AuthService;
 import com.soulcode.goserviceapp.service.UsuarioService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,13 +17,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping(value = "/auth")
 public class AuthController {
 
+
     @Autowired
-    private UsuarioService usuarioService;
+    private AuthService authService;
 
     @GetMapping(value = "/login")
     public ModelAndView login(@RequestParam(name = "error", required = false) String error) {
         ModelAndView mv = new ModelAndView("login");
-        if (error != null) {
+        if(error != null) {
             mv.addObject("errorMessage", "Erro ao autenticar no sistema. Verifique suas credenciais.");
         }
         return mv;
@@ -33,20 +35,36 @@ public class AuthController {
         return "cadastroCliente";
     }
 
-    @GetMapping(value = "/password/new")
-    public String alterarSenha() {
-        return "alterarSenha";
-    }
-
     @PostMapping(value = "/cadastro")
     public String cadastrarCliente(Cliente cliente, RedirectAttributes attributes) {
         try {
-            usuarioService.createUser(cliente);
+            authService.createCliente(cliente);
             attributes.addFlashAttribute("successMessage", "Novo cliente cadastrado com sucesso!");
             return "redirect:/auth/login";
         } catch (Exception ex) {
             attributes.addFlashAttribute("errorMessage", "Erro ao cadastrar novo cliente.");
             return "redirect:/auth/cadastro";
         }
+    }
+
+    @GetMapping(value = "/password/new")
+    public String alterarSenha() {
+        return "alterarSenha";
+    }
+
+    @PostMapping(value = "/password/new")
+    public String updatePassword(
+            @RequestParam(name = "senhaAtual") String senhaAtual,
+            @RequestParam(name = "senhaNova") String senhaNova,
+            Authentication authentication,
+            RedirectAttributes attributes
+    ) {
+        try {
+            authService.updatePassword(authentication, senhaAtual, senhaNova);
+            attributes.addFlashAttribute("successMessage", "Senha alterada com sucesso!");
+        } catch (Exception ex){
+            attributes.addFlashAttribute("errorMessage", "Erro ao alterar a senha.");
+        }
+        return "redirect:/auth/password/new";
     }
 }
